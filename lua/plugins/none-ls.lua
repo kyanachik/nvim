c04f3ea0e973b7f8c6d74f8bb4ff69baa1f23997
Keith Yanachik <keith@yanachik.com>
2025-01-31 20:54:29 -0500

    Added formatting and linting.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,18 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvimtools/none-ls-extras.nvim" },
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.completion.spell,
+				require("none-ls.diagnostics.eslint"),
+			},
+		})
 
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
-  end,
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
+	end,
 }
